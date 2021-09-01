9aa3cc396acbdfcbbc5e4dfe43d9a2a7a3ac920f
Ciro Mota <ciro.mota@gmail.com>
2021-09-01 14:59:47 -0300

    Correção de dezenas de erros e finalização de script secundário.

--- files/Pos_Install_Debian.sh
+++ files/Pos_Install_Debian.sh
@@ -11,11 +11,9 @@
 ## LICENÇA:
 ###		  GPLv3. <https://github.com/ciro-mota/Pos-Instalacao-Ubuntu/blob/master/LICENSE>
 ## CHANGELOG:
-### 		Última edição 18/08/2021. <https://github.com/ciro-mota/Pos-Instalacao-Ubuntu/commits/master>
+### 		Última edição 01/09/2021. <https://github.com/ciro-mota/Pos-Instalacao-Ubuntu/commits/master>
 
 ### Para calcular o tempo gasto na execução do script, use o comando "time ./Pos_Install.sh".
-
-### ESTE SCRIPT ESTÁ EM CONSTRUÇÃO, É ESPERADO FALHAS. TENTE NÃO UTILIZÁ-LO.
 
 # ------------------------------------------------------------------------------------------------------------- #
 # -------------------------------------------- VARIÁVEIS E REQUISITOS ----------------------------------------- #
@@ -23,18 +21,19 @@
 ### PPA's e links de download dinâmicos.
 url_lutris="http://download.opensuse.org/repositories/home:/strycore/Debian_10/"
 url_ppa_lutris="https://download.opensuse.org/repositories/home:/strycore/Debian_10/Release.key"
+url_key_brave="https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg"
+url_ppa_brave="https://brave-browser-apt-release.s3.brave.com/"
 url_dck_key="https://download.docker.com/linux/debian/gpg"
 url_ppa_dck="https://download.docker.com/linux/debian"
-url_key_brave="https://brave-browser-apt-release.s3.brave.com/brave-core.asc"
-url_ppa_brave="https://brave-browser-apt-release.s3.brave.com/"
-url_key_code="https://packages.microsoft.com/keys/microsoft.asc"
-url_ppa_code="https://packages.microsoft.com/repos/vscode"
-url_ppa_obs="ppa:obsproject/obs-studio"
+url_key_only="hkp://keyserver.ubuntu.com:80"
+url_ppa_only="https://download.onlyoffice.com/repo/debian"
 url_jopplin="https://raw.githubusercontent.com/laurent22/joplin/dev/Joplin_install_and_update.sh"
+url_flathub="https://flathub.org/repo/flathub.flatpakrepo"
 url_dbox="https://www.dropbox.com/download?dl=packages/ubuntu/dropbox_2020.03.04_amd64.deb"
+url_code="https://az764295.vo.msecnd.net/stable/3866c3553be8b268c8a7f8c0482c0c0177aa8bfa/code_1.59.1-1629375198_amd64.deb"
 url_tviewer="https://download.teamviewer.com/download/linux/teamviewer_amd64.deb"
 url_firefox="https://ftp.mozilla.org/pub/firefox/releases/91.0.1/linux-x86_64/pt-BR/firefox-91.0.1.tar.bz2"
-# url_backup"https://github.com/ciro-mota/conf-backup.git"
+# url_backup="https://github.com/ciro-mota/conf-backup.git"
 
 
 ### Programas para instalação.
@@ -42,14 +41,13 @@ apps=(brave-browser
 	celluloid 
 	containerd.io 
 	cowsay 
-	cups 
 	default-jre 
 	docker-ce 
-	docker-ce-cli 
 	exfat-fuse 
 	fastboot 
 	ffmpegthumbnailer 
-	flatpak 
+	file-roller 	
+	firmware-linux-nonfree 
 	font-manager 
 	fortune 
 	gir1.2-gtop-2.0 
@@ -68,21 +66,21 @@ apps=(brave-browser
 	libasound2-plugins:i386 
 	libsdl2-2.0-0:i386 
 	libsqlite3-0:i386 
-	lm-sensors 
 	lolcat
 	lutris 
-	mesa-vulkan-drivers 
 	neofetch 
 	network-manager-gnome 
-	p7zip-full 
 	plymouth 
 	plymouth-themes 
 	obs-studio 
 	onlyoffice-desktopeditors 
-	qbittorrent 
+	openprinting-ppds 
+	printer-driver-hpcups 
+	printer-driver-pxljr 
+	seahorse  
 	terminator 
-	vim-runtime
-	winbind 
+	transmission 
+	vim-runtime 
 	zsh)
 	
 flatpak=(com.spotify.Client 
@@ -116,7 +114,10 @@ diretorio_downloads="$HOME/Downloads/programas"
 ### Check se a distribuição é a correta.
 if [[ $(lsb_release -cs) = "bullseye" ]]
 then
+	echo ""
+	echo ""
 	echo -e "\e[32;1mDebian Testing. Prosseguindo com o script...\e[m"
+	echo ""
 	echo ""
 else
 	echo -e "\e[31;1mDistribuição não homologada para uso com este script.\e[m"
@@ -126,7 +127,9 @@ fi
 # ------------------------------------------------------------------------------------------------------------- #
 # -------------------------------------- ATIVANDO CONTRIB E NON-FREE ------------------------------------------ #
 sudo sed -i 's/#.*$//;/^$/d' /etc/apt/sources.list
+sudo sed -i 's/bullseye/bookworm/g' /etc/apt/sources.list
 sudo sed -i 's/main/main non-free contrib/g' /etc/apt/sources.list
+sudo apt update -y
 
 # ------------------------------------------------------------------------------------------------------------- #
 # ------------------------------------------ APLICANDO REQUISITOS --------------------------------------------- #
@@ -138,9 +141,9 @@ sudo apt-get install \
     apt-transport-https \
     ca-certificates \
     curl \
+	git \
     gnupg-agent \
-    software-properties-common \
-    gnome-software-plugin-flatpak -y
+    software-properties-common -y
 
 ### Adicionando repositórios de terceiros.
 echo "deb $url_lutris ./" | sudo tee /etc/apt/sources.list.d/lutris.list
@@ -151,15 +154,9 @@ echo \
   "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] "$url_ppa_dck" \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
-wget -qO- "$url_key_code" | gpg --dearmor > packages.microsoft.gpg
-sudo install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
-sudo sh -c 'echo "deb [arch=amd64 signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] '$url_ppa_code' stable main" > /etc/apt/sources.list.d/vscode.list'
-rm -f packages.microsoft.gpg
-
-sudo add-apt-repository "$url_ppa_obs"
-
-curl -fsSL "$url_key_brave" | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] $url_ppa_brave stable main"
+sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg "$url_key_brave"
+echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] \
+	$url_ppa_brave stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list > /dev/null
 
 sudo apt-key adv --keyserver $url_key_only --recv-keys CB2DE8E5
 echo "deb $url_ppa_only squeeze main" | sudo tee -a /etc/apt/sources.list.d/onlyoffice.list
@@ -170,7 +167,10 @@ sudo apt update -y && sudo apt upgrade -y
 # ------------------------------------------------------------------------------------------------------------- #
 # ------------------------------------------------- EXECUÇÃO -------------------------------------------------- #
 ### Instalação do ambiente gráfico minimo.
-sudo apt install gnome-core xorg gdm3 --no-install-recommends -y
+sudo apt install gnome-core \
+		xorg \
+		gdm3 \
+		gnome-software-plugin-flatpak --no-install-recommends -y
 
 ### Instalação de programas.
 for nome_do_app in ${apps[@]}; do
@@ -182,27 +182,30 @@ for nome_do_app in ${apps[@]}; do
 done
 
 ### Instalação de pacotes Flatpak.
-flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+flatpak remote-add --if-not-exists flathub $url_flathub
 
 for nome_do_flatpak in ${flatpak[@]}; do
   if ! flatpak list | grep -q $nome_do_flatpak; then
-    flatpak install flathub --system "$nome_do_flatpak" -y
+    sudo flatpak install flathub --system "$nome_do_flatpak" -y
   fi
 done
 
+### Instalação do Jopplin
+wget -O - $url_jopplin | bash
+
 ### Download de programas .deb.
 mkdir -p "$diretorio_downloads"
+wget -cq --show-progress "$url_code" 	-P "$diretorio_downloads"
 wget -cq --show-progress "$url_dbox"    -P "$diretorio_downloads"
 wget -cq --show-progress "$url_tviewer" -P "$diretorio_downloads"
 
 ### Instalando pacotes .deb.
 sudo apt install -y $diretorio_downloads/*.deb
 
-### Limpando pasta temporária dos downloads.
-sudo rm $diretorio_downloads/*.* -f
-
-### Instalação do Jopplin
-wget -O - $url_jopplin | bash
+### Instalação extensões do Code.
+for code_ext in ${code_extensions[@]}; do
+    code --install-extension "$code_ext" 2> /dev/null
+done
 
 ### Instalação do Firefox Release.
 wget -cq --show-progress "$url_firefox"   -P "$diretorio_downloads"
@@ -224,44 +227,6 @@ StartupNotify=true
 EOF'
 
 sudo chown $(whoami):$(whoami) $HOME/.local/share/applications/firefox-stable.desktop
-
-### Procedimentos e otimizações.
-sudo sed -i "s/NoDisplay=true/NoDisplay=false/g" /etc/xdg/autostart/*.desktop
-sudo sh -c 'echo "# Menor uso de Swap" >> /etc/sysctl.conf'
-sudo sh -c 'echo vm.swappiness=10 >> /etc/sysctl.conf'
-sudo sh -c 'echo vm.vfs_cache_pressure=50 >> /etc/sysctl.conf'
-sudo mv /etc/network/interfaces /etc/network/interfaces.old
-sudo usermod -aG docker $(whoami)
-sudo sed -i 's/GRUB_TIMEOUT=5/GRUB_TIMEOUT=0/g' /etc/default/grub
-sed -i 's/GRUB_CMDLINE_LINUX_DEFAULT="splash"/GRUB_CMDLINE_LINUX_DEFAULT="splash quiet"/g' /etc/default/grub
-sudo update-grub
-sed -i 's/logo-text-version-128.png/logo-text-64.png/g' /etc/gdm3/greeter.dconf-defaults
-sudo dpkg-reconfigure gdm3
-
-### Instalação de ícones, temas e configurações.
-if [ -d "$HOME/.icons" ]; then
-  echo "Pasta já existe."
-else
-  mkdir $HOME/.icons
-fi
-
-if [ -d "$HOME/.themes" ]; then
-  echo "Pasta já existe."
-else
-  mkdir $HOME/.themes
-fi
-
-# git clone https://github.com/ciro-mota/conf-backup.git
-
-# mv $HOME/conf-backup/Dracula-Blue $HOME/.themes
-# mv $HOME/conf-backup/Yaru-Deepblue-dark $HOME/.themes
-# mv $HOME/conf-backup/Flat-Remix-Blue-Dark $HOME/.icons
-# mv $HOME/conf-backup/volantes_cursors $HOME/.icons
-# mv $HOME/conf-backup/neofetch/config.conf $HOME/.config/neofetch
-# mv $HOME/conf-backup/terminator $HOME/.config/terminator
-# mv $HOME/conf-backup/.zsh_aliases $HOME
-# mv $HOME/conf-backup/.zshrc $HOME
-# mv $HOME/conf-backup/.vim $HOME
 
 # ------------------------------------------------------------------------------------------------------------- #
 # ------------------------------------------------- PÓS-INSTALAÇÃO -------------------------------------------- #
@@ -298,16 +263,76 @@ sudo apt purge firefox-esr -y
 sudo apt autoremove
 sudo apt autoclean
 
+### Limpando pasta temporária dos downloads.
+sudo rm $diretorio_downloads/*.* -f
+
+### Reiniciará o PC ou encerrará a execução do scrip.
+echo -e "Digite S para reiniciar, ou N para sair do Script:"
+read  reinicia
+
+case $reinicia in
+    S|s)
+    	sudo shutdown -r now
+		;;
+    N|n)
+    	exit 0
+		;;s
+esac
+
 # ------------------------------------------------------------------------------------------------------------- #
 # -------------------------------------------------- PÓS-REBOOT ----------------------------------------------- #
-### Linhas que deverão ser executadas após o reboot e carregamento do ambiente gráfico.
+### Segundo script que deverá ser executado após o reboot e carregamento do ambiente gráfico.
+### Copie, descomente e salve em um novo arquivo .sh.
+
+### #!/usr/bin/env bash
+
+### Instalação de ícones, temas e configurações pessoais. Remova este trecho e pule para a próxima sessão.
+# if [ -d $HOME/.icons ]
+# then
+#   echo "Pasta já existe."
+# else
+#   mkdir -p $HOME/.icons
+# fi
+
+# if [ -d $HOME/.themes ]
+# then
+#   echo "Pasta já existe."
+# else
+#   mkdir -p $HOME/.themes
+# fi
+
+# git clone https://github.com/ciro-mota/conf-backup.git
+
+# cp -r $HOME/conf-backup/Dracula-Blue $HOME/.themes
+# cp -r $HOME/conf-backup/Yaru-Deepblue-dark $HOME/.themes
+# cp -r $HOME/conf-backup/Flat-Remix-Blue-Dark $HOME/.icons
+# cp -r $HOME/conf-backup/volantes_cursors cp -r $HOME/.icons
+# cp -r $HOME/conf-backup/neofetch/config.conf $HOME/.config/neofetch
+# cp -r $HOME/conf-backup/terminator $HOME/.config/terminator
+# cp -r $HOME/conf-backup/.zsh_aliases $HOME
+# cp -r $HOME/conf-backup/.zshrc $HOME
+# cp -r $HOME/conf-backup/.vim $HOME
 
 # gsettings set org.gnome.desktop.interface gtk-theme 'Dracula-Blue'
 # gsettings set org.gnome.desktop.interface icon-theme 'Flat-Remix-Blue-Dark'
 # gsettings set org.gnome.shell.extensions.user-theme name 'Yaru-Deepblue-dark'
 # gsettings set org.gnome.desktop.interface cursor-theme 'volantes_cursors'
 # gsettings set org.gnome.desktop.wm.preferences button-layout ':minimize,close'
-# sudo flatpak --system override org.telegram.desktop --filesystem=/home/$USER/.icons/:ro
-# sudo flatpak --system override com.spotify.Client --filesystem=/home/$USER/.icons/:ro
-# sudo flatpak --system override com.valvesoftware.Steam --filesystem=/home/$USER/.icons/:ro
+
+### Procedimentos e otimizações.
+# sudo sed -i "s/NoDisplay=true/NoDisplay=false/g" /etc/xdg/autostart/*.desktop
+# sudo sh -c 'echo "# Menor uso de Swap" >> /etc/sysctl.conf'
+# sudo sh -c 'echo vm.swappiness=10 >> /etc/sysctl.conf'
+# sudo sh -c 'echo vm.vfs_cache_pressure=50 >> /etc/sysctl.conf'
+# sudo mv /etc/network/interfaces /etc/network/interfaces.old
+# sudo usermod -aG docker $(whoami)
+# sudo sed -i 's/GRUB_TIMEOUT=5/GRUB_TIMEOUT=0/g' /etc/default/grub
+# sudo sed -ie 's/GRUB_CMDLINE_LINUX_DEFAULT="\(.*\)"/GRUB_CMDLINE_LINUX_DEFAULT="\1 splash"/' /etc/default/grub
+# sudo update-grub
+# sudo sed -i 's/logo-text-version-64.png/logo-text-64.png/g' /etc/gdm3/greeter.dconf-defaults
+# sudo dpkg-reconfigure gdm3
+# sudo flatpak --system override org.telegram.desktop --filesystem=$HOME/.icons/:ro
+# sudo flatpak --system override com.spotify.Client --filesystem=$HOME/.icons/:ro
+# sudo flatpak --system override com.valvesoftware.Steam --filesystem=$HOME/.icons/:ro
 # ln -s /usr/share/hunspell/* ~/.config/Code/Dictionaries
+# sudo dpkg-reconfigure gdm3
