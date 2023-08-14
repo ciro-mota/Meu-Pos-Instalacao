ff879e61597e6b5acb8cf706e1baa0e4f41540d5
ciro-mota <ciro.mota@gmail.com>
2023-08-14 17:00:42 -0300

    fix: Removed personalization section

--- debian/Pos_Install_Debian.sh
+++ debian/Pos_Install_Debian.sh
@@ -11,7 +11,7 @@
 ## LICENÇA:
 ###		  GPLv3. <https://github.com/ciro-mota/Meu-Pos-Instalacao/blob/main/LICENSE>
 ## CHANGELOG:
-### 		Última edição 09/08/2023. <https://github.com/ciro-mota/Meu-Pos-Instalacao/commits/main>
+### 		Última edição 14/08/2023. <https://github.com/ciro-mota/Meu-Pos-Instalacao/commits/main>
 
 ### Para calcular o tempo gasto na execução do script, use o comando "time ./Pos_Install_Debian.sh".
 
@@ -453,67 +453,5 @@ esac
 
 # ------------------------------------------------------------------------------------------------------------- #
 # -------------------------------------------------- PÓS-REBOOT ----------------------------------------------- #
-### Segundo script que deverá ser executado após o reboot e carregamento do ambiente gráfico.
-### Copie, descomente e salve em um novo arquivo .sh.
-
-### #!/usr/bin/env bash
-
-### Instalação de ícones, temas, fonte e configurações pessoais. Remova este trecho e pule para a próxima seção.
-# theme (){
-
-# curl -s https://api.github.com/repos/lassekongo83/adw-gtk3/releases/latest | grep "browser_download_url.*tar.xz" | \
-# cut -d : -f 2,3 | tr -d \" | \
-# wget -P "$diretorio_downloads" -i-
-# tar xf "$diretorio_downloads"/*.tar.xz -C "$HOME"/.local/share/themes
-
-# gsettings set org.gnome.desktop.interface gtk-theme 'adw-gtk3-dark' && gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
-
-# }
-
-# icon (){
-
-# wget -cqO- https://git.io/papirus-icon-theme-install | DESTDIR="$HOME/.local/share/icons" sh
-# wget -cqO- https://git.io/papirus-folders-install | sh
-# gsettings set org.gnome.desktop.interface icon-theme 'Papirus'
-# papirus-folders -C bluegrey --theme Papirus
-# rm "$HOME"/.local/share/icons/ePapirus-Dark -rf
-# rm "$HOME"/.local/share/icons/ePapirus -rf
-# rm "$HOME"/.local/share/icons/Papirus-Dark -rf
-# rm "$HOME"/.local/share/icons/Papirus-Light -rf
-
-# }
-
-# if [ -d "$HOME"/.local/share/icons ]
-# then
-#   echo -e "Pasta já existe.\n"
-#   echo -e "Instalando..."
-#   icon
-# else
-#   mkdir -p "$HOME"/.local/share/icons
-#   echo -e "Instalando..."
-#   icon
-# fi
-
-# if [ -d "$HOME"/.local/share/themes ]
-# then
-#   echo -e "Pasta já existe.\n"
-#   echo -e "Instalando..."
-#   theme
-# else
-#   mkdir -p "$HOME"/.local/share/themes
-#   echo -e "Instalando..."
-#   theme
-# fi
-
-# sudo flatpak override --system --filesystem=/usr/share/icons/:ro
-# sudo flatpak override --system --filesystem=xdg-data/icons:ro
-# sudo flatpak override --filesystem=xdg-data/themes:ro
-# sudo flatpak override --system --env=XCURSOR_PATH=/run/host/user-share/icons:/run/host/share/icons
-# gsettings set org.gnome.Terminal.Legacy.Settings confirm-close false
-# gsettings set org.gnome.desktop.default-applications.terminal exec terminator
-# gsettings set org.gnome.desktop.wm.preferences button-layout 'appmenu:minimize,maximize,close'
-# gsettings set org.gnome.desktop.input-sources sources "[('xkb', 'br')]"
-# xdg-mime default org.gnome.Nautilus.desktop inode/directory 
-
-# sudo systemctl stop packagekit
-# sudo systemctl disable packagekit
+### As configurações a seguir não podem ser executadas em um ambiente TTY, deste modo será necessário reiniciar
+### e executar o script "Pos_Install_Debian_B" após carregamento do ambiente gráfico.
